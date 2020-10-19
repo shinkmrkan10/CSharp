66601c86d7af32b5db7972776958128a43968eb3
shinkmrkan10 <shinkmrkan10@gmail.com>
2020-10-19 09:42:24 +0900

    /Test02 added

---
+++ Wpf/Test02/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Test02
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.dataGrid.ItemsSource = new[]
+            {
+                    new[] { "A", "B", "C", "D"},
+                    new[] { "A", "B", "C", "D"},
+                    new[] { "A", null, "C", "D"},
+                    new[] { "A", "B", "", "D"},
+                    new[] { "A", "B", "C"},
+            };
+
+        }
+    }
+}
